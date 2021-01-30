e5f1a3c966e4c2e40efde858d0289bfaa9f1e962
Eashvar Balachandran <eashvar.balachandran@uoit.net>
2021-01-30 17:45:11 -0500

    Hayroll task

--- Assets/_Scripts/Tasks.cs
+++ Assets/_Scripts/Tasks.cs
@@ -6,14 +6,25 @@ public class Tasks : MonoBehaviour
 {
     // Start is called before the first frame update
     PlayerController player;
+    public GameObject Hayroll;
+
+    static public bool didHayroll = false;
+    bool[] taskList;
     void Start()
     {
         player = GameObject.FindObjectOfType<PlayerController>();
+        taskList = new bool[6];
+
+        for(int i =0; i < 6;i++)
+        {
+            taskList[i] = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Bring Axe to Farmer Task
         if(player.hitInfo.collider != null)
         {
             if(player.hitInfo.collider.tag == "Farmer")
@@ -21,11 +32,20 @@ public class Tasks : MonoBehaviour
                 if(Input.GetButtonDown("Interact") && player.currentItem.name == "Axe")
                 {
                     Debug.Log("Respect Fam");
+                    taskList[0] = true;
                     Destroy(player.currentItem);
                     player.currentlyHolding = false;
                     player.currentItem = new GameObject();
                 }
             }
+        }
+
+        //Roll Hayroll to Spot *collision code in chekCollision.cs*
+        if(didHayroll == true)
+        {
+            taskList[1] = true;
+            Debug.Log("task complete");
+            didHayroll = false;
         }
     }
 }
---
+++ Assets/_Scripts/checkCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkCollision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col) 
+    {
+        if(col.gameObject.name == "hayRoll")
+        {
+            Tasks.didHayroll = true;
+            col.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
+            col.GetComponent<Rigidbody>().useGravity = false;
+            Destroy(this.gameObject);
+        }
+    }
+}
