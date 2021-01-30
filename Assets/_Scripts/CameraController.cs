bdbd18505b56a39bde22294260c142106d4f4862
Lillian Fan <ziqi.fan@ontariotechu.net>
2021-01-29 21:30:20 -0500

    Player Controller & Camera Controller Finished

---
+++ Assets/_Scripts/CameraController.cs
@@ -0,0 +1,70 @@
+﻿// Camera shake: https://gist.github.com/nbrew/52df13e93e3170f73080
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float rotateSpeed;
+    public Transform player;
+
+    float xRotate = 0f;
+
+    private float timer = 0.0f;
+    public float shakingSpeed = 0.18f;
+    public float shakingAmount = 0.2f;
+    public float midp = 2.0f;
+
+    void Start()
+    {
+        xRotate = 0f;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // rotate camera with mouse & controller
+        float x = Input.GetAxis("Mouse X") * rotateSpeed;
+        float y = Input.GetAxis("Mouse Y") * rotateSpeed;
+
+        xRotate -= y;
+        xRotate = Mathf.Clamp(xRotate, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotate, 0f, 0f);
+        player.Rotate(Vector3.up * x);
+
+        // make camera shake when moving
+        float waveslice = 0.0f;
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs(h) == 0 && Mathf.Abs(v) == 0)
+        {
+            timer = 0.0f;
+        }
+        else
+        {
+            waveslice = Mathf.Sin(timer);
+            timer = timer + shakingSpeed;
+            if (timer > Mathf.PI * 2)
+            {
+                timer = timer - (Mathf.PI * 2);
+            }
+        }
+
+        Vector3 tranlateLocation = transform.localPosition;
+
+        if (waveslice != 0)
+        {
+            float translateChange = waveslice * shakingAmount;
+            float totalAxes = Mathf.Abs(h) + Mathf.Abs(v);
+            totalAxes = Mathf.Clamp(totalAxes, 0.0f, 1.0f);
+            translateChange = totalAxes * translateChange;
+            tranlateLocation.y = midp + translateChange;
+        }
+        else
+        {
+            tranlateLocation.y = midp;
+        }
+
+        transform.localPosition = tranlateLocation;
+    }
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿// Player movement: https://www.youtube.com/watch?v=_QajrabyTJc&ab_channel=Brackeys
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Transform groundCheck;
+    public float fallingSpeed = -18f;
+    public float jumpHeight = 4f;
+
+    CharacterController controller;
+    Vector3 velocity;
+    float groundDis = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    void Start()
+    {
+        controller = this.GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // check if player on ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDis, groundMask);
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // move with keyboard & controller
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 direct = transform.right * x + transform.forward * y;
+        controller.Move(direct * speed * Time.deltaTime);
+
+        // jump with keybboard & controller
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * fallingSpeed);
+        }
+
+        velocity.y += fallingSpeed * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
