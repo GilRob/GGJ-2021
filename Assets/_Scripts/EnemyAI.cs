23eb11a15d9c8f491ce999f8549b5fd5d47a0014
Lillian Fan <ziqi.fan@ontariotechu.net>
2021-01-30 00:23:07 -0500

    Basic Path-finding done

---
+++ Assets/_Scripts/EnemyAI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform player;
+    NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = this.gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(player.transform.position);
+    }
+}
