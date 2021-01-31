b5c2fd07818b5e6063773fa57f7cacb6befda5f1
Safa <safa.nazir@uoit.net>
2021-01-30 22:42:22 -0500

    menu buttons work

--- Assets/_Scripts/Buttons.cs
+++ Assets/_Scripts/Buttons.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartButton : MonoBehaviour
+public class Buttons : MonoBehaviour
 {
 
     public void changeScene()
     {
         SceneManager.LoadScene("MainScene");
+    }
+
+    public void endGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is closed");
     }
 }
