70be512aff505716d6fd63756cd06999c24b5e81
Gil Robern <gil.robern@uoit.net>
2021-02-14 23:22:31 -0500

    Water Retriever
    Objects thrown into water will respawn on dock. Removed shadows on almost everything.

---
+++ Assets/WaterRetriever.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterRetriever : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Interactable")
+        {
+            Debug.Log("water retrieved");
+            other.gameObject.transform.position = new Vector3(-81.4f, 1.0f, -236.58f);
+        }
+    }
+}
